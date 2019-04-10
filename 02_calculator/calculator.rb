3049ef00ebda6369fb050dd54f548fa4eba11a6a
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2019-04-10 14:07:50 -0700

    completes calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  sum = 0
+  arr.each { |num| sum += num }
+  sum
+end
+
+def multiply(*nums)
+  result = 1
+  nums.each { |num| result *= num }
+  result
+end
+
+def power(num1, num2)
+  num1 ** num2
+end
+
+def factorial(num)
+  start = 1
+  start.upto(num) { |int| start *= int }
+  start
+end
